00da2ea1a71a864a7fed36b9ddd013104db5403f
Viphase <9374143431@mail.ru>
2025-12-07 10:42:14 +0300

    major-updated-mediapipe-tracking-model

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from numpy import array, int32
-from cv2 import (VideoCapture, minEnclosingCircle, putText, imshow, 
+from cv2 import (VideoCapture, minEnclosingCircle, putText, imshow,
 waitKey, destroyAllWindows, FONT_HERSHEY_SIMPLEX, LINE_AA)
 from math import acos, degrees
 from utils.tracking import MediaPipeFacade
@@ -7,31 +7,32 @@ from utils.geometry import Vector
 
 
 def finger_dist(first, second, results) -> float:
-    if results is None or results.multi_hand_landmarks is None or len(results.multi_hand_landmarks) == 0:
+    if results is None or not results.hand_landmarks:
         return 0
-    
-    x1, y1 = results.multi_hand_landmarks[0].landmark[first].x, results.multi_hand_landmarks[0].landmark[first].y
-    x2, y2 = results.multi_hand_landmarks[0].landmark[second].x, results.multi_hand_landmarks[0].landmark[second].y
-    return ((x1 - x2)**2 + (y1 - y2) **2)**.5
+
+    lm = results.hand_landmarks[0]
+    x1, y1 = lm[first].x, lm[first].y
+    x2, y2 = lm[second].x, lm[second].y
+    return ((x1 - x2)**2 + (y1 - y2)**2)**.5
 
 
 def is_pistol(results) -> bool:
-    if results is None or results.multi_hand_landmarks is None or len(results.multi_hand_landmarks) == 0:
+    if results is None or not results.hand_landmarks:
         return False
 
+    lm = results.hand_landmarks[0]
     open = 0
     closed = 0
-    finger_starts = [1, 5, 9, 13, 17]
-    
-    for i in finger_starts:
+
+    for i in [1, 5, 9, 13, 17]:
         prev = None
         for j in range(i, i+3):
             if j + 1 > 20:
                 break
-            current = Vector(results.multi_hand_landmarks[0].landmark[j+1].x - 
-                            results.multi_hand_landmarks[0].landmark[j].x, 
-                            results.multi_hand_landmarks[0].landmark[j+1].y - 
-                            results.multi_hand_landmarks[0].landmark[j].y)
+            current = Vector(
+                lm[j+1].x - lm[j].x,
+                lm[j+1].y - lm[j].y
+            )
             if prev is not None:
                 angle = degrees(acos((current * prev) / (current.dist() * prev.dist())))
                 if 0 <= angle <= 40:
@@ -39,29 +40,29 @@ def is_pistol(results) -> bool:
                 else:
                     closed += 1
             prev = current
-    return open >= 7 and closed >= 3
+    return open >= 6 and closed >= 2
 
-    # return (finger_dist(4, 8, results) > 0.08 
-    #         and finger_dist(12, 0, results) < 0.3  
-    #         and finger_dist(16, 0, results) < 0.3 
-    #         and finger_dist(20, 0, results) < 0.3)   --- deprecated
-    
+
+def arm_angle(results):
+    ...
+
 
 def is_fist(results, shape) -> bool:
-    if results is None or results.multi_hand_landmarks is None or len(results.multi_hand_landmarks) == 0:
+    if results is None or not results.hand_landmarks:
         return False
-    
+
+    lm = results.hand_landmarks[0]
     points = []
-    for mark in results.multi_hand_landmarks[0].landmark:
+
+    for mark in lm:
         points.append([mark.x * shape[1], mark.y * shape[0]])
     points = array(points, dtype=int32)
 
-    x1, y1 = results.multi_hand_landmarks[0].landmark[0].x * shape[1], results.multi_hand_landmarks[0].landmark[0].y * shape[0]
-    x2, y2 = results.multi_hand_landmarks[0].landmark[5].x * shape[1], results.multi_hand_landmarks[0].landmark[5].y * shape[0]
-   
-    ws = ((x1 - x2)**2 + (y1 - y2) **2)**.5
+    x1, y1 = lm[0].x * shape[1], lm[0].y * shape[0]
+    x2, y2 = lm[5].x * shape[1], lm[5].y * shape[0]
+
+    ws = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** .5
     _, r = minEnclosingCircle(points)
-    
     return (2 * r / ws) > 1.6
 
 
@@ -82,10 +83,10 @@ def main():
         else:
             text = "NO GUN"
             color = (0, 0, 255)
-        
-        putText(frame, text, (50, 50), FONT_HERSHEY_SIMPLEX, 1, color, 2, LINE_AA)
 
+        putText(frame, text, (50, 50), FONT_HERSHEY_SIMPLEX, 1, color, 2, LINE_AA)
         imshow("testik", frame)
+
         key = waitKey(1) & 0xFF
         if key == 27 or key == ord('q'):
             mp_facade.close()
--- utils/tracking.py
+++ utils/tracking.py
@@ -1,45 +1,63 @@
 from cv2 import cvtColor, resize, COLOR_RGB2BGR, COLOR_BGR2RGB
-from mediapipe import solutions
+from mediapipe.tasks.python import vision
+from mediapipe import Image, ImageFormat
+from mediapipe.tasks import python as mp_tasks
 from numpy import fliplr
+
 
 class MediaPipeFacade:
     def __init__(self):
-        self.mp_hands = solutions.hands
-        self.hands = self.mp_hands.Hands(
-            static_image_mode=False,
-            max_num_hands=4,
-            min_detection_confidence=0.5,
-            min_tracking_confidence=0.5
+        base_options_hands = mp_tasks.BaseOptions(
+            model_asset_path="./models/hand_landmarker.task"
         )
-        self.mp_pose = solutions.pose
-        self.pose = self.mp_pose.Pose(
-            static_image_mode=False,
-            min_detection_confidence=0.5,
-            min_tracking_confidence=0.5
+
+        base_options_pose = mp_tasks.BaseOptions(
+            model_asset_path="./models/pose_landmarker.task"
         )
-        self.mp_drawing = solutions.drawing_utils
+
+        self.hands = vision.HandLandmarker.create_from_options(
+            vision.HandLandmarkerOptions(
+                base_options=base_options_hands,
+                num_hands=4,
+                min_hand_detection_confidence=0.5,
+                min_hand_presence_confidence=0.5
+            )
+        )
+
+        self.pose = vision.PoseLandmarker.create_from_options(
+            vision.PoseLandmarkerOptions(
+                base_options=base_options_pose,
+                num_poses=4,
+                min_pose_detection_confidence=0.5,
+                min_pose_presence_confidence=0.5
+            )
+        )
+
+        self.mp_drawing = None
 
     def process_frame(self, frame, debug: bool = False):
         img = fliplr(frame)
         img = cvtColor(img, COLOR_BGR2RGB)
-        img = resize(img, (1280, 720))
+        img = resize(img, (1920, 1080))
 
-        results_hands = self.hands.process(img)
-        results_pose = self.pose.process(img)
+        mp_image = Image(image_format=ImageFormat.SRGB, data=img)
+
+        results_hands = self.hands.detect(mp_image)
+        results_pose = self.pose.detect(mp_image)
 
         if debug:
-            if results_hands.multi_hand_landmarks:
-                for hand_landmarks in results_hands.multi_hand_landmarks:
-                    self.mp_drawing.draw_landmarks(
-                        img, hand_landmarks, self.mp_hands.HAND_CONNECTIONS)
+            if results_hands.hand_landmarks:
+                for hand in results_hands.hand_landmarks:
+                    for lm in hand:
+                        px = int(lm.x * img.shape[1])
+                        py = int(lm.y * img.shape[0])
+                        img[py-2:py+2, px-2:px+2] = [255, 0, 0]
 
             if results_pose.pose_landmarks:
-                self.mp_drawing.draw_landmarks(
-                    img, results_pose.pose_landmarks, self.mp_pose.POSE_CONNECTIONS)
-        
+                for pose in results_pose.pose_landmarks:
+                    for lm in pose:
+                        px = int(lm.x * img.shape[1])
+                        py = int(lm.y * img.shape[0])
+                        img[py-2:py+2, px-2:px+2] = [0, 255, 0]
+
         return cvtColor(img, COLOR_RGB2BGR), results_hands, results_pose
-
-
-    def close(self):
-        self.hands.close()
-        self.pose.close()
